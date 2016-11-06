0190bc318a7e525ba60ff0b5536c7f3ac40679b0
gameknife <kaimingyi@163.com>
2016-11-06 22:38:21 +0800

    create webgl canvas & glclear

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// initial
+window.onload = function(){
+    win = window;
+    var canvas = bid('canvas');
+    glw = window.parent.glw;
+    glw.initGL(canvas);
+    if(!glw.ready){console.log('initialize error'); return;}
+
+    // clear with a red color
+    glw.clear([0.7, 0.1, 0.1, 1.0], 1.0);
+};
+
+function bid(id){return document.getElementById(id);}
