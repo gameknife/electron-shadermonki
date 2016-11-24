25772c757abc84d1040eded4a36c86fac5b5fc0a
gameknife <kaimingyi@163.com>
2016-11-25 00:10:43 +0800

    normal decoded

--- test/loader-osgjs.js
+++ test/loader-osgjs.js
@@ -7,7 +7,7 @@ var loader_osgjs = require('../lib/loader/loader-osgjs.js');
 test('loader-osgjs', function (t) {
     t.plan(1);
 
-    loader_osgjs.load( "./res/mesh/box100.osgjs", "./res/mesh/box100.osgjs.bin", function(res) {
+    loader_osgjs.load( "./res/mesh/quad2.osgjs", "./res/mesh/quad2.osgjs.bin", function(res) {
         //t.message( res );
     } );
 
