3b2c61c36b41a690b4c4c3309335469ed3ca6de9
gameKnife <kaimingyi@163.com>
2016-11-28 21:56:54 +0800

    material repo builded, support opcity map, need: refactor

--- res/shader/base_fs.glsl
+++ res/shader/base_fs.glsl
@@ -4,7 +4,8 @@ _MainTex ('Font Texture', 2D) = 'white'
 
 */
 precision mediump float;
-uniform sampler2D texture;
+uniform sampler2D _MainTex;
+uniform sampler2D _AlphaTex;
 varying vec2 vTexCoord;
 varying vec3 vNormal;
 
@@ -19,9 +20,11 @@ void main(){
     vec4 samplerColor1 = vec4(frac(vTexCoord.x + _TIME),frac(vTexCoord.y + _TIME),0,1);
     vec4 samplerColor2 = vec4(vNormal * vec3(0.5,0.5,0.5) + vec3(0.5,0.5,0.5),1);
     float ndotl = max(0.0, dot(_LIGHTDIR.xyz, vNormal));
-    vec4 samplerColor = texture2D(texture, vTexCoord.xy);
+    vec4 samplerColor = texture2D(_MainTex, vTexCoord.xy);
+    vec4 alphaSamplerColor = texture2D(_AlphaTex, vTexCoord.xy);
     samplerColor *= samplerColor;
-    if(samplerColor.a < 0.5) discard;
+    samplerColor.a *= alphaSamplerColor.r;
+    if(samplerColor.a < 0.25) discard;
     samplerColor = samplerColor * (ndotl + vec4(0.3,0.4,0.5,1.0) * (vNormal.y * 0.4 + 0.6));
     samplerColor.a = 1.0;
     gl_FragColor = sqrt(samplerColor);
