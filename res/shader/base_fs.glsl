3dd827f09dab59b346aad68e9fa902d3f5044c1e
gameknife <kaimingyi@163.com>
2017-01-01 01:15:54 +0800

    shadow map integrate

--- res/shader/base_fs.glsl
+++ res/shader/base_fs.glsl
@@ -6,17 +6,37 @@ _MainTex ('Font Texture', 2D) = 'white'
 precision mediump float;
 uniform sampler2D _MainTex;
 uniform sampler2D _AlphaTex;
+uniform sampler2D _GlobalShadowMap;
 varying vec2 vTexCoord;
 varying vec3 vNormal;
+varying vec4 vLightHPos;
 
 uniform float _TIME;
 uniform vec4 _LIGHTDIR;
 
+float DecodeFloatRGBA(vec4 enc){
+    vec4 kDecodeDot = vec4(1.0,1.0/255.0,1.0/65025.0,1.0/160581375.0);
+    return dot(enc, kDecodeDot);
+}
 float frac(float t)
 {
     return t - floor(t);
 }
 void main(){
+
+    // shadow
+    vec3 light_hpos = vLightHPos.xyz / vLightHPos.w;
+
+    vec4 shadowmap = texture2D(_GlobalShadowMap, light_hpos.xy * 0.5 + vec2(0.5,0.5));
+    float depthA = DecodeFloatRGBA(shadowmap);
+    if(depthA < light_hpos.z - 0.003)
+    {
+        depthA = 0.0;
+    }
+    else
+    {
+        depthA = 1.0;
+    }
     vec4 samplerColor1 = vec4(frac(vTexCoord.x + _TIME),frac(vTexCoord.y + _TIME),0,1);
     vec4 samplerColor2 = vec4(vNormal * vec3(0.5,0.5,0.5) + vec3(0.5,0.5,0.5),1);
     float ndotl = max(0.0, dot(_LIGHTDIR.xyz, vNormal));
@@ -25,7 +45,8 @@ void main(){
     samplerColor *= samplerColor;
     samplerColor.a *= alphaSamplerColor.r * alphaSamplerColor.a;
     if(samplerColor.a < 0.05) discard;
-    samplerColor = samplerColor * (ndotl + vec4(0.3,0.4,0.5,1.0) * (vNormal.y * 0.4 + 0.6));
+    samplerColor = samplerColor * (ndotl * depthA + vec4(0.3,0.4,0.5,1.0) * (vNormal.y * 0.4 * depthA + 0.6));
     samplerColor.a = 1.0;
+    //gl_FragColor = vec4(depthA,depthA,depthA,1.0);
     gl_FragColor = sqrt(samplerColor);
 }
--- res/shader/base_vs.glsl
+++ res/shader/base_vs.glsl
@@ -2,15 +2,20 @@ attribute vec2 texcoord;
 attribute vec3 normal;
 attribute vec3 position;
 uniform mat4 _MVP;
+uniform mat4 _MV;
 uniform mat4 _M2W;
+uniform mat4 _MVP_LIGHT;
 
 varying vec2 vTexCoord;
 varying vec3 vNormal;
+varying vec4 vLightHPos;
 
 void main(){
     vTexCoord = (position * 0.075).xy;
     vTexCoord = texcoord;
     vNormal = (_M2W * vec4(normal, 0.0)).xyz;
+    vLightHPos = _MV * vec4(position, 1.0);
+    vLightHPos = _MVP_LIGHT * vLightHPos;
     /*gl_Position = vec4(position, 1.0);*/
     gl_Position = _MVP * vec4(position, 1.0);
 }
