98c9e26e65e9200a381165d88c06d3b58e425f98
gameknife <kaimingyi@163.com>
2017-02-09 16:49:21 +0800

    clean

--- renderer.js
+++ renderer.js
@@ -11,8 +11,6 @@ const mouse             = require('./lib/gk-mouseorbit');
 const resMgr            = require('./lib/gk-resmgr');
 const resPanel          = require('./lib/gk-respanel');
 const Framework         = require('./lib/gk-framework');
-const exporter_obj      = require('./lib/exporter/exporter-obj');
-const packageBuilder    = require('./lib/util/package-builder');
 const path              = require('path');
 
 // initial
@@ -157,10 +155,6 @@ window.onload = function(){
         }
     }
 
-    let expBtn = bid('exporter');
-    expBtn.onclick = function () {
-        exporter_obj.exportNode( holderGameObject.transform, current_proj );
-    }
 };
 
 function refresh_playbtn( element_id, element_class, swt ) {
@@ -175,52 +169,3 @@ function refresh_playbtn( element_id, element_class, swt ) {
 
 function bid(id){return document.getElementById(id);}
 
-var vsp = "\
-attribute vec2 texcoord;\r\
-attribute vec3 normal;\r\
-attribute vec3 position;\r\
-uniform mat4 _MVP;\r\
-uniform mat4 _M2W;\r\
-\r\
-\r\
-varying vec2 vTexCoord;\r\
-varying vec3 vNormal;\r\
-\r\
-void main(){\r\
-\r\
-    vTexCoord = (position * 0.075).xy;\r\
-    //vTexCoord = texcoord;\r\
-    vNormal = (_M2W * vec4(normal, 0.0)).xyz;\r\
-    /*gl_Position = vec4(position, 1.0);*/\r\
-    gl_Position = _MVP * vec4(position, 1.0);\r\
-}\r\
-";
-
-var fsp = "\
-/*\r\
-\r\
-_MainTex ('Font Texture', 2D) = 'white'\r\
-\r\
-*/\r\
-precision mediump float;\r\
-uniform sampler2D texture;\r\
-varying vec2 vTexCoord;\r\
-varying vec3 vNormal;\r\
-\r\
-\r\
-uniform float _TIME;\r\
-uniform vec4 _LIGHTDIR;\r\
-float frac(float t)\r\
-{\r\
-    return t - floor(t);\r\
-}\r\
-void main(){\r\
-    vec4 samplerColor1 = vec4(frac(vTexCoord.x + _TIME),frac(vTexCoord.y + _TIME),0,1);\r\
-    vec4 samplerColor2 = vec4(vNormal * vec3(0.5,0.5,0.5) + vec3(0.5,0.5,0.5),1);\r\
-    float ndotl = max(0.0, dot(_LIGHTDIR.xyz, vNormal));\r\
-    vec4 samplerColor = texture2D(texture, vTexCoord.xy);\r\
-    samplerColor = samplerColor * (ndotl + vec4(0.3,0.4,0.5,1.0) * (vNormal.y * 0.4 + 0.6));\r\
-    samplerColor.a = 1.0;\r\
-    gl_FragColor = sqrt(samplerColor);\r\
-}\
-";
