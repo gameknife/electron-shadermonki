ee3174241cb5b337f6edde476b8b73596bdbf538
gameKnife <kaimingyi@163.com>
2017-03-19 18:24:22 +0800

    shadow sample & fbo retarget

--- res/shader/base_fs.glsl
+++ res/shader/base_fs.glsl
@@ -15,6 +15,9 @@ varying vec4 vLightHPos;
 uniform float _TIME;
 uniform vec4 _LIGHTDIR;
 
+#define BIA 0.0003
+#define SHADOWMAP_SIZE (1.0 / 1024.0)
+
 float DecodeFloatRGBA(vec4 enc){
     vec4 kDecodeDot = vec4(1.0,1.0/255.0,1.0/65025.0,1.0/160581375.0);
     return dot(enc, kDecodeDot);
@@ -23,22 +26,46 @@ float frac(float t)
 {
     return t - floor(t);
 }
+
+float snoise(vec2 co){
+return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float sampleShadow(vec3 light_hpos)
+{
+    float noise = fract(_TIME);
+    float noiseMap = snoise(vTexCoord.xy + vec2(noise,noise)) * 1.0;
+    vec2 normal = vec2(noiseMap, 1.0 - noiseMap);
+    //normal = normalize(normal);
+    vec4 shadow4;
+    
+    shadow4.x = sign(texture2D(_GlobalShadowMap, light_hpos.xy * 0.5 
+    + vec2(0.5,0.5) 
+    + reflect( normal, vec2(1,0)) 
+    * SHADOWMAP_SIZE).r - (light_hpos.z * 0.5 + 0.5) + BIA);
+    
+        shadow4.y = sign(texture2D(_GlobalShadowMap, light_hpos.xy * 0.5 
+    + vec2(0.5,0.5) 
+    + reflect( normal, vec2(-1,0)) 
+    * SHADOWMAP_SIZE).r - (light_hpos.z * 0.5 + 0.5) + BIA);
+        shadow4.z = sign(texture2D(_GlobalShadowMap, light_hpos.xy * 0.5 
+    + vec2(0.5,0.5) 
+    + reflect( normal, vec2(0,1)) 
+    * SHADOWMAP_SIZE).r - (light_hpos.z * 0.5 + 0.5) + BIA);
+        shadow4.w = sign(texture2D(_GlobalShadowMap, light_hpos.xy * 0.5 
+    + vec2(0.5,0.5) 
+    + reflect( normal, vec2(0,-1)) 
+    * SHADOWMAP_SIZE).r - (light_hpos.z * 0.5 + 0.5) + BIA);
+    
+    return max(0.0, dot(shadow4, vec4(0.25)) );
+}
 void main(){
 
     // shadow
     vec3 light_hpos = vLightHPos.xyz / vLightHPos.w;
-
-    vec4 shadowmap = texture2D(_GlobalShadowMap, light_hpos.xy * 0.5 + vec2(0.5,0.5));
-    //float depthA = DecodeFloatRGBA(shadowmap);
-    float depthA = shadowmap.r;
-    if(depthA < (light_hpos.z * 0.5 + 0.5)  - 0.003)
-    {
-        depthA = 0.0;
-    }
-    else
-    {
-        depthA = 1.0;
-    }
+    float shadow = sampleShadow(light_hpos);
+    
+    // occlusion
 
     vec4 samplerColor1 = vec4(frac(vTexCoord.x + _TIME),frac(vTexCoord.y + _TIME),0,1);
     vec4 samplerColor2 = vec4(vNormal * vec3(0.5,0.5,0.5) + vec3(0.5,0.5,0.5),1);
@@ -48,7 +75,7 @@ void main(){
     samplerColor *= samplerColor;
     samplerColor.a *= alphaSamplerColor.r * alphaSamplerColor.a;
     if(samplerColor.a < 0.05) discard;
-    samplerColor = samplerColor * (ndotl * depthA + vec4(0.3,0.4,0.5,1.0) * (vNormal.y * 0.4 * depthA + 0.6));
+    samplerColor = samplerColor * (ndotl * shadow + vec4(0.2,0.3,0.7,1.0) * (vNormal.y * 0.4 * shadow + 0.6));
     samplerColor.a = 1.0;
     //gl_FragColor = vec4(depthA,depthA,depthA,1.0);
     gl_FragColor = sqrt(samplerColor);
